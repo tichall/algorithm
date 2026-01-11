2a466e332bc9734e94592855931dd7099a34409e
Seoyeon Lee <152502639+tichall@users.noreply.github.com>
2026-01-11 23:54:17 +0900

    [level 2] Title: 기능개발, Time: 2.91 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        // 100 - 작업 진도 = 남은 진도
+        // 남은 양을 해결하는데 걸리는 일수 
+        // => 남은 진도 % 속도 == 0 ? 남은 진도 / 속도 : 남은 진도 / 속도 + 1
+        // 각 작업마다 걸리는 일수를 모두 구한다.
+        // 첫 번째 작업이 끝난 날 -> 해당 일수보다 적거나 같은 작업이 전부 끝난다.
+        // 나머지 
+        Deque<Integer> days = new ArrayDeque<>();
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = 0; i < progresses.length; i++) {
+            int remain = 100 - progresses[i];
+            int day = remain % speeds[i] == 0 ? remain / speeds[i] : remain / speeds[i] + 1;
+            days.add(day);
+        }
+        
+        while (!days.isEmpty()) {
+            int functions = 1; // 첫 번째 작업은 이미 꺼낼 것이므로 1로 시작
+            int currentDay = days.pop(); // 기준이 되는 배포일
+
+            // 큐가 비어있지 않고, 다음 작업이 기준일(currentDay) 이내에 끝나는 동안 반복
+            while (!days.isEmpty() && days.peekFirst() <= currentDay) {
+                days.pop();
+                functions++;
+            }
+
+            answer.add(functions);
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
