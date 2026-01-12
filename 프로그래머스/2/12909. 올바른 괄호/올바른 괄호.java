06acbb1da910f141552f72bbab7265ce778999d7
Seoyeon Lee <152502639+tichall@users.noreply.github.com>
2026-01-12 23:32:47 +0900

    [level 2] Title: 올바른 괄호, Time: 18.72 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -5,11 +5,11 @@ class Solution {
         // 스택을 쓴다면
         // 닫는 괄호인 경우 스택에 담긴 최상위 요소가 여는 괄호인지 확인
         // 여는 괄호라면 pop, 아니라면 false 반환
-        Stack<String> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
         
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
-                stack.push(String.valueOf(s.charAt(i)));
+                stack.push(s.charAt(i));
             } else {
                 if (stack.isEmpty()) {
                     return false;
