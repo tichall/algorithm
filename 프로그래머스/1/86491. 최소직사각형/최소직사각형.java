6f11c6d902da03e6d45650727d3b0f338d895114
Seoyeon Lee <152502639+tichall@users.noreply.github.com>
2026-01-16 22:37:42 +0900

    [level 1] Title: 최소직사각형, Time: 1.59 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,30 +1,13 @@
 class Solution {
-    public int solution(int[][] sizes) {
+    public int solution(int[][] sizes) {        
+        int length = 0;
+        int height = 0;
         
-        // 가장 작은 지갑을 찾아야 함
-        // 가로 길이 최대를 찾는다.
-        // 세로 길이 최대를 찾는다.
-        // 가로, 세로 중 최대값 -> 가장 긴 길이로 확정
-        // 나머지 한 변의 길이를 어떻게 확정지어야 하지?
-        
-        int firstMax = 0;
-        int secondMax = 0;
-        
-        for (int i = 0; i < sizes.length; i++) {
-            int first = 0;
-            int second = 0;
-            for (int j = 0; j < sizes[0].length; j++) {
-                if (sizes[i][j] > first) {
-                    second = first;
-                    first = sizes[i][j];
-                } else {
-                    second = sizes[i][j];
-                }
-            }
-            if(first > firstMax) firstMax = first;
-            if(second > secondMax) secondMax = second;
+        for (int[] size : sizes) {
+            length = Math.max(length, Math.max(size[0], size[1]));
+            height = Math.max(height, Math.min(size[0], size[1]));
         }
         
-        return firstMax * secondMax;
+        return length * height;
     }
 }
