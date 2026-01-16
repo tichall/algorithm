b4181ed84405a14572c688415b87fa6148581478
Seoyeon Lee <152502639+tichall@users.noreply.github.com>
2026-01-16 18:42:00 +0900

    [level 2] Title: 프로세스, Time: 1.40 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,35 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        // location에 해당하는 프로세스를 기억하고 있어야 함
-        // deque를 사용해 프로세스를 큐에 넣었다 뺐다 했을 때 프로세스의 위치로 원하는 프로세스를 찾을 수 없음..
-        // 1. 우선순위와 원래 프로세스 번호를 함께 저장하는 방법?
-        // 2. 실제 큐를 조작하지 않고 바로 구하는 방법? -> 너무 복잡한 것 같다
-        // 같은 우선순위를 가지는 프로세스 간의 순서를 구하는 방법
-        
-        Deque<int[]> que = new ArrayDeque<>();
+        // 1. 우선순위를 내림차순으로 관리할 우선순위 큐
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // 2. 프로세스의 {우선순위, 원래 인덱스}를 담을 큐
+        Queue<int[]> queue = new LinkedList<>();
+
         for (int i = 0; i < priorities.length; i++) {
-            que.add(new int[]{priorities[i], i});
+            pq.add(priorities[i]);
+            queue.add(new int[]{priorities[i], i});
         }
-        
+
         int count = 0;
-        
-        while (true) {
-            int[] currentProcess = que.pop();
-            int queSize = que.size();
-            
-            for (int[] process : que) {
-                if (currentProcess[0] < process[0]) {
-                    que.add(currentProcess);
-                    break;
-                } else continue;
-            }
-            
-            // 현재 프로세스가 실행된 경우
-            if (queSize == que.size()) {
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            // 현재 꺼낸 프로세스의 우선순위가 가장 높은 우선순위인지 확인
+            if (current[0] == pq.peek()) {
                 count++;
-                if (currentProcess[1] == location) return count;
+                pq.poll(); // 실행 완료되었으므로 우선순위 큐에서도 제거
+
+                // 찾고자 하는 위치의 프로세스라면 리턴
+                if (current[1] == location) {
+                    return count;
+                }
+            } else {
+                // 더 높은 우선순위가 있다면 다시 뒤로 보냄
+                queue.add(current);
             }
         }
+        return count;
     }
 }
