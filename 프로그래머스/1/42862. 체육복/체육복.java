72c36ac8c62d365f4dbc40ef508c9b7bf641e317
Seoyeon Lee <152502639+tichall@users.noreply.github.com>
2026-01-19 23:46:19 +0900

    [level 1] Title: 체육복, Time: 0.71 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        // 여벌 있는 학생이 도난당한 경우를 제외시켜야 함
+        int len = lost.length;
+        int answer= n - len;
+        Set<Integer> clothes = new HashSet<Integer>();
+        boolean[] visited = new boolean[len];
+        
+        for(int num : reserve) {
+            clothes.add(num);
+        }
+        
+        Arrays.sort(lost);
+        for(int i = 0; i < len; i++) {
+            if(clothes.contains(lost[i])) {
+                answer++;
+                visited[i] = true;
+                clothes.remove(lost[i]);
+            }
+        }
+
+
+        for(int i = len - 1; i >= 0; i--) {
+            if (visited[i]) continue;
+            
+            if(clothes.contains(lost[i] + 1)) {
+                clothes.remove(lost[i] + 1);
+                answer++;
+            } else if(clothes.contains(lost[i] - 1)) {
+                clothes.remove(lost[i] - 1);
+                answer++;
+            }
+        }
+
+
+        return answer;
+    }
+}
