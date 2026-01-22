1eae4491b486cab188588b60bd2c1376f25ee857
Seoyeon Lee <152502639+tichall@users.noreply.github.com>
2026-01-22 21:51:29 +0900

    [level 2] Title: 구명보트, Time: 10.60 ms, Memory: 57.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int answer = 0;
+        
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+                right--;
+                answer++;
+            } else {
+                right--;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
