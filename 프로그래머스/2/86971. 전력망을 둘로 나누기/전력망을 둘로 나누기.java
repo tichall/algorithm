ff5d5cbadf33f991bed9627d165b8cad1a978c1a
Seoyeon Lee <152502639+tichall@users.noreply.github.com>
2026-02-09 20:30:21 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.37 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,55 +4,50 @@ class Solution {
     static boolean[] visited;
     public int solution(int n, int[][] wires) {
         // 전선 중 하나를 끊어 2개로 분할
-        // 이 2개의 전력망의 송전탑 개수 차이를 반환
+        // 이 2개의 전력망의 송전탑 개수 차이를 반환 (가장 적은 값)
         // 모든 전선을 끊어보고 노드 수를 세야 한다
         // 그래프 자료 구조를 실제로 만들어야 하는가?
         
-        // 그래프를 만든다.
-        // 모든 간선에 대해
-        
-        // 그래프 만들기
+        // 하나를 끊는다. -> 송전탑 하나에서 시작된 전력망 하나를 끝까지 탐색한다.
+        // 전체 n에서 빼면 송전탑 개수의 차이
         int minAbs = n;
-        List<Integer>[] graph = new ArrayList[n + 1];
+        List<Integer>[] linked = new ArrayList[n + 1];
+        
         for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+            linked[i] = new ArrayList<>();
         }
         
-        for (int i = 0; i < wires.length; i++) {
-            int a = wires[i][0];
-            int b = wires[i][1];
+        for (int[] wire : wires) {
+            int x = wire[0];
+            int y = wire[1];
             
-            graph[a].add(b);
-            graph[b].add(a);
+            linked[x].add(y);
+            linked[y].add(x);
         }
         
         for (int[] wire : wires) {
             visited = new boolean[n + 1];
-            int nodes = dfs(wire[0], wire, graph);
-            minAbs = Math.min(minAbs, Math.abs(n - 2 * nodes));
+            
+            int startNode = wire[0];
+            visited[startNode] = true;
+            visited[wire[1]] = true;
+            
+            minAbs = Math.min(minAbs, Math.abs(n - 2 * dfs(startNode, linked)));
         }
+        
         return minAbs;
     }
     
-    public int dfs(int node, int[] wire, List<Integer>[] graph) {
-        int cutA = wire[0];
-        int cutB = wire[1];
+    public int dfs(int startNode, List<Integer>[] linked) {
+        int nodes = 1;
         
-        int cnt = 1;
-        visited[node] = true;
-        
-        for (int next : graph[node]) {
-            if (visited[next]) continue;
-            
-            // 지금 끊은 간선인 경우
-            if ((node == cutA && next == cutB) ||
-                (node == cutB && next == cutA)) {
-                continue;
-            }
-            
-            cnt += dfs(next, wire, graph);
+        for (int node : linked[startNode]) {
+            if (!visited[node]) {
+                visited[node] = true;
+                nodes += dfs(node, linked);
+            } 
         }
         
-        return cnt;
+        return nodes;
     }
 }
