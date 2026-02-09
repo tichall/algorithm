020fbe279a17235a413617f5e189d468df0bd6ef
Seoyeon Lee <152502639+tichall@users.noreply.github.com>
2026-02-09 23:33:34 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.31 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,15 +1,8 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] visited;
+    boolean[] visited;
     public int solution(int n, int[][] wires) {
-        // 전선 중 하나를 끊어 2개로 분할
-        // 이 2개의 전력망의 송전탑 개수 차이를 반환 (가장 적은 값)
-        // 모든 전선을 끊어보고 노드 수를 세야 한다
-        // 그래프 자료 구조를 실제로 만들어야 하는가?
-        
-        // 하나를 끊는다. -> 송전탑 하나에서 시작된 전력망 하나를 끝까지 탐색한다.
-        // 전체 n에서 빼면 송전탑 개수의 차이
         int minAbs = n;
         List<Integer>[] linked = new ArrayList[n + 1];
         
